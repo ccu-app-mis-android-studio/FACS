c7ef982e52ed7b4d5cc90c2400295a634cc92cbc
xipeng1718 <nxp1718@gmail.com>
2019-03-31 01:20:26 +0800

    new 3/31

--- app/src/main/java/com/example/jessl/alarm/Schedule.java
+++ app/src/main/java/com/example/jessl/alarm/Schedule.java
@@ -4,15 +4,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.CalendarView;
-
-import org.w3c.dom.Text;
+import android.content.Intent;
+import android.view.View;
+import java.util.Calendar;
+import android.widget.Button;
 
 public class Schedule extends AppCompatActivity {
 
-
+    private Button add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule2);
+        add = (Button) findViewById(R.id.Add);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendarEvent = Calendar.getInstance();
+                Intent i = new Intent(Intent.ACTION_EDIT);
+                i.setType("vnd.android.cursor.item/event");
+                i.putExtra("beginTime", calendarEvent.getTimeInMillis());
+                i.putExtra("allDay", true);
+                i.putExtra("rule", "FREQ=YEARLY");
+                i.putExtra("endTime", calendarEvent.getTimeInMillis() + 60 * 60 * 1000);
+                i.putExtra("title", "Add Event");
+                startActivity(i);
+            }
+        });
     }
 }
